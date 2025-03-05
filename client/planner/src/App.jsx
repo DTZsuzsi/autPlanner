59ecf3261de7fa440564f14ccfe5ebf9c794783b
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-03-05 13:53:56 +0100

    registration page

--- client/planner/src/App.jsx
+++ client/planner/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./scenes/HomePage.jsx";
+import RegistrationPage from "./scenes/RegistrationPage.jsx";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<HomePage />} />
+                    <Route path="/register" element={<RegistrationPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
--- client/planner/src/scenes/RegistrationPage.jsx
+++ client/planner/src/scenes/RegistrationPage.jsx
@@ -1,6 +1,9 @@
+import SignUpForm from "../components/auth/SignUpForm.jsx";
+
 function RegistrationPage() {
     return (
         <div className="flex flex-col items-center my-2 mx-auto  rounded-lg min-w-[280px] max-w-[600px]">
+<SignUpForm/>
         </div>
             )
             }
