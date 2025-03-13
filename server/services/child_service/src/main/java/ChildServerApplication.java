928f65c21ee2503589890060d4a804cb771712fe
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-03-13 11:09:48 +0100

    child service setup- database, appl.porperties, main class

---
+++ server/services/child_service/src/main/java/ChildServerApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableRabbit
+public class ChildServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildServerApplication.class, args);
+    }
+
+}
+
