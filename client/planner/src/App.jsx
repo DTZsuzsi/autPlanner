ae0cae6806e463c3a84b3abd91905c7936b4fb38
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-03-19 11:05:03 +0100

    tailwind fixed, topnavbar started

--- client/planner/src/App.jsx
+++ client/planner/src/App.jsx
@@ -4,11 +4,13 @@ import HomePage from "./scenes/HomePage.jsx";
 import RegistrationPage from "./scenes/RegistrationPage.jsx";
 import MyAccount from "./scenes/MyAccount.jsx";
 import NewChildForm from "./components/children/NewChildForm.jsx";
+import TopNavBar from "./components/navigation/TopNavBar.jsx";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <TopNavBar/>
                 <Routes>
                     <Route path="/login" element={<HomePage />} />
                     <Route path="/register" element={<RegistrationPage />} />
