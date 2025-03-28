1a129eb607f650f26f195f1f730b1752e22c6878
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-03-28 13:31:22 +0100

    planner controller done

--- server/services/planner_service/src/main/java/com/codecool/controller/PlannerController.java
+++ server/services/planner_service/src/main/java/com/codecool/controller/PlannerController.java
@@ -1,7 +1,43 @@
 package com.codecool.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.DTO.PlannerDTO.NewPlannerDTO;
+import com.codecool.DTO.PlannerDTO.PlannerDTO;
+import com.codecool.service.PlannerService;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/planner")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/planner")
 public class PlannerController {
+private final PlannerService plannerService;
+
+public PlannerController(PlannerService plannerService) {
+    this.plannerService = plannerService;
+}
+
+@GetMapping("/")
+    public List<PlannerDTO> getAllPlanners() {
+    return plannerService.findAll();
+}
+
+@GetMapping("/{id}")
+    public PlannerDTO getPlannerById(@PathVariable Long id) {
+    return  plannerService.findById(id);
+}
+
+@PostMapping("/")
+    public long createPlanner(@RequestBody NewPlannerDTO plannerDTO) {
+    return plannerService.createPlanner(plannerDTO);
+}
+
+@PatchMapping("/")
+    public long updatePlanner(@RequestBody PlannerDTO plannerDTO) {
+    return plannerService.updatePlanner(plannerDTO);
+}
+
+@DeleteMapping("/{id}")
+    public boolean deletePlannerById(@PathVariable Long id) {
+    return plannerService.deletePlanner(id);
+}
 }
