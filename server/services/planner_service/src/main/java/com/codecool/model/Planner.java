729ba0400a09b6c3b609353caf5acd6447150c70
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-03-28 11:37:50 +0100

    planner model done

--- server/services/planner_service/src/main/java/com/codecool/model/Planner.java
+++ server/services/planner_service/src/main/java/com/codecool/model/Planner.java
@@ -1,9 +1,8 @@
 package com.codecool.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Planner {
@@ -11,4 +10,48 @@ public class Planner {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @OneToOne
+    private Child child;
+    @OneToMany
+    private List<Task> tasks;
+
+    public Planner() {
+    }
+
+    public Planner(String name, Child child) {
+        this.name = name;
+        this.child = child;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
