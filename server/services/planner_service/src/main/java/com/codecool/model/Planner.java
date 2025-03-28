283a591141e0e3cf9afaf9a2561e77d0496dce0a
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-03-28 10:53:59 +0100

    tasks populated

--- server/services/planner_service/src/main/java/com/codecool/model/Planner.java
+++ server/services/planner_service/src/main/java/com/codecool/model/Planner.java
@@ -1,4 +1,14 @@
 package com.codecool.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Planner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
 }
